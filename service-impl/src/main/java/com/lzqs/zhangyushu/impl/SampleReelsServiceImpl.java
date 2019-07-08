6397dddfd7c3b7c60b39462e6764c1afced29cb0
Lastyear <836985507@qq.com>
2019-07-08 09:57:32 +0800

    201907061420

--- service-impl/src/main/java/com/lzqs/zhangyushu/impl/SampleReelsServiceImpl.java
+++ service-impl/src/main/java/com/lzqs/zhangyushu/impl/SampleReelsServiceImpl.java
@@ -1,19 +1,21 @@
 package com.lzqs.zhangyushu.impl;
 
-import com.lzqs.zhangyushu.dao.CommonFileMapper;
-import com.lzqs.zhangyushu.dao.UserMapper;
-import com.lzqs.zhangyushu.entity.CommonFile;
-import com.lzqs.zhangyushu.entity.SampleReels;
-import com.lzqs.zhangyushu.dao.SampleReelsMapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.lzqs.zhangyushu.dao.*;
+import com.lzqs.zhangyushu.entity.*;
 import com.lzqs.zhangyushu.service.CommonFileService;
 import com.lzqs.zhangyushu.service.SampleReelsService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import common.ConfigBeanProp;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -25,34 +27,139 @@ import java.util.List;
  */
 @Service
 public class SampleReelsServiceImpl extends ServiceImpl<SampleReelsMapper, SampleReels> implements SampleReelsService {
-    @Resource
-    CommonFileService commonFileService;
-    @Resource
-    SampleReelsMapper sampleReelsMapper;
-    @Resource
-    UserMapper userMapper;
 
+
+    @Resource
+    private SampleReelsMapper sampleReelsMapper;
+
+    @Resource
+    private CommonFileMapper commonFileMapper;
+
+    @Resource
+    private ConfigBeanProp configBeanProp;
+
+    @Resource
+    private ProductMapper productMapper;
+
+    @Resource
+    private ProductImgMapper productImgMapper;
+
+    @Resource
+    private CommentMapper commentMapper;
+
+    @Resource
+    private UserMapper userMapper;
+
+    /**
+     * 根据用户id查看他的绘馆
+     *
+     * @param userId
+     * @return
+     */
     @Override
-    public SampleReels saveSample(HttpServletRequest request, Long userId, int status, String sampleReelsName, String description) {
-
-        List<CommonFile> commonFiles = commonFileService.uploadFile(request);
-        if (commonFiles.isEmpty()){
-            return null;
+    public List<Map<String, Object>> listSampleReels(Long userId) {
+        QueryWrapper<SampleReels> sampleReelsQueryWrapper = new QueryWrapper<>();
+        sampleReelsQueryWrapper.lambda().eq(SampleReels::getUserId, userId).eq(SampleReels::getStatus, 1);
+        List<SampleReels> sampleReelsList = sampleReelsMapper.selectList(sampleReelsQueryWrapper);
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        if (!sampleReelsList.isEmpty()) {
+            sampleReelsList.forEach(sampleReels -> {
+                Map<String, Object> map = new HashMap<>();
+                map.put("sampleReelsId", sampleReels.getSampleReelsId());
+                map.put("sampleReelsName", sampleReels.getSampleReelsName());
+                map.put("samoleReelsDesc", sampleReels.getSamoleReelsDesc());
+                map.put("likeNum", sampleReels.getLikeNum());
+                map.put("commentNum", sampleReels.getCommentNum());
+                map.put("sampleReelsCover", configBeanProp.getFile_url() + commonFileMapper.selectById(sampleReels.getSampleReelsCover()).getFilePath());
+                map.put("createTime", sampleReels.getCreateTime());
+                mapList.add(map);
+            });
         }
-        CommonFile commonFile = commonFiles.get(0);
-        SampleReels sampleReels = new SampleReels();
-        sampleReels.setUserId(userId);
-        sampleReels.setUserName(userMapper.selectById(userId).getUserName());
-        sampleReels.setSampleReelsName(sampleReelsName);
-        sampleReels.setSampleReelsCover(commonFile.getId());
-        sampleReels.setSamoleReelsDesc(description);
-        sampleReels.setCommentNum(0);
-        sampleReels.setLikeNum(0);
-        sampleReels.setViewNum(Long.valueOf(0));
-        sampleReels.setCreateTime(LocalDateTime.now());
-        sampleReels.setStatus(status);
+        return mapList;
+    }
 
-        sampleReelsMapper.insert(sampleReels);
-        return sampleReels;
+    /**
+     * 根据作品集id查看所有作品
+     *
+     * @param sampleReelsId
+     * @return
+     */
+    @Override
+    public List<Map<String, Object>> listProduction(Long sampleReelsId) {
+        QueryWrapper<Product> productQueryWrapper = new QueryWrapper<>();
+        productQueryWrapper.lambda().eq(Product::getSampleReelsId, sampleReelsId);
+        List<Product> productList = productMapper.selectList(productQueryWrapper);
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        if (!productList.isEmpty()) {
+            productList.forEach(product -> {
+                Map<String, Object> map = new HashMap<>();
+                map.put("productionId", product.getProductionId());
+                map.put("productionName", product.getProductionName());
+                map.put("likeNum", product.getLikeNum());
+                map.put("commentNum", product.getCommentNum());
+                map.put("createTime", product.getCreateTime());
+                QueryWrapper<ProductImg> productImgQueryWrapper = new QueryWrapper<>();
+                productImgQueryWrapper.lambda().eq(ProductImg::getProductId, product.getProductionId());
+                List<ProductImg> productImgList = productImgMapper.selectList(productImgQueryWrapper);
+                List<Map<String, Object>> imgList = new ArrayList<>();
+                if (!productImgList.isEmpty()) {
+                    productImgList.forEach(productImg -> {
+                        Map<String, Object> imgMap = new HashMap<>();
+                        imgMap.put("filePath", configBeanProp.getFile_url() + commonFileMapper.selectById(productImg.getProductImg()).getFilePath());
+                        imgList.add(imgMap);
+                    });
+                    map.put("imgList", imgList);
+
+                }
+            });
+        }
+        return mapList;
+    }
+
+
+    /**
+     * \根据作品id查看作品详情
+     *
+     * @param productionId
+     * @return
+     */
+    @Override
+    public Map<String, Object> getProduction(Long productionId) {
+        Product product = productMapper.selectById(productionId);
+        Map<String, Object> map = new HashMap<>();
+        map.put("productionName", product.getProductionName());
+        map.put("likeNum", product.getLikeNum());
+        map.put("commentNum", product.getCommentNum());
+        //查看作品图片
+        QueryWrapper<ProductImg> productImgQueryWrapper = new QueryWrapper<>();
+        productImgQueryWrapper.lambda().eq(ProductImg::getProductId, product.getProductionId());
+        List<ProductImg> productImgList = productImgMapper.selectList(productImgQueryWrapper);
+        List<Map<String, Object>> imgList = new ArrayList<>();
+        if (!productImgList.isEmpty()) {
+            productImgList.forEach(productImg -> {
+                Map<String, Object> imgMap = new HashMap<>();
+                imgMap.put("filePath", configBeanProp.getFile_url() + commonFileMapper.selectById(productImg.getProductImg()).getFilePath());
+                imgList.add(imgMap);
+            });
+            map.put("imgList", imgList);
+            map.put("productDesc", product.getProductDesc());
+            //查看作品评论
+            QueryWrapper<Comment> commentQueryWrapper = new QueryWrapper<>();
+            commentQueryWrapper.lambda().eq(Comment::getBeCommentId, product.getProductionId()).eq(Comment::getStatus, 1);
+            List<Comment> commentList = commentMapper.selectList(commentQueryWrapper);
+            List<Map<String, Object>> commentContentList = new ArrayList<>();
+            if (!commentList.isEmpty()) {
+                commentList.forEach(comment -> {
+                    Map<String, Object> commentContentMap = new HashMap<>();
+                    commentContentMap.put("comment", comment.getCommentContent());
+                    User user = userMapper.selectById(comment.getUserId());
+                    commentContentMap.put("userName", user.getUserName());
+                    commentContentMap.put("userHead", user.getUserHead());
+                    commentContentList.add(commentContentMap);
+                });
+                map.put("commentContentList", commentContentList);
+            }
+        }
+        return map;
     }
 }
--- service/src/main/java/com/lzqs/zhangyushu/service/SampleReelsService.java
+++ service/src/main/java/com/lzqs/zhangyushu/service/SampleReelsService.java
@@ -4,6 +4,8 @@ import com.lzqs.zhangyushu.entity.SampleReels;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -15,5 +17,13 @@ import javax.servlet.http.HttpServletRequest;
  */
 public interface SampleReelsService extends IService<SampleReels> {
 
-    SampleReels saveSample(HttpServletRequest request, Long userId, int status, String sampleReelsName, String description);
+
+    //根据用户id查看他的绘馆
+    List<Map<String,Object>> listSampleReels(Long userId);
+
+    //根据作品集id查看所有作品
+    List<Map<String,Object>>  listProduction(Long sampleReelsId);
+
+    //根据作品id查看作品详情
+    Map<String,Object>  getProduction(Long productionId);
 }
